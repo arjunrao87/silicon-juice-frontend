f8736d21cb6f92aaf17231a5f3caf8c074be6d83
Arjun Rao <rao.arjun87@gmail.com>
2024-03-09 14:57:13 -0500

    feat: add buymeacoffee

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' embeds.beehiiv.com gc.zgo.at googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' embeds.beehiiv.com gc.zgo.at googletagmanager.com cdnjs.buymeacoffee.com www.buymeacoffee.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src embeds.beehiiv.com gc.zgo.at googletagmanager.com
+  frame-src embeds.beehiiv.com gc.zgo.at googletagmanager.com cdnjs.buymeacoffee.com www.buymeacoffee.com
 `;
 
 const securityHeaders = [
--- pages/index.tsx
+++ pages/index.tsx
@@ -749,6 +749,18 @@ export default function Home() {
             </p>
           </section>
         </main>
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="arjunrao87"
+          data-description="Support me on Buy me a coffee!"
+          data-message=""
+          data-color="#40DCA5"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
         <Footer />
       </div>
     </>
